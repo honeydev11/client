b6680e34264802c7a77e8b259e1d8d3a71d52787
honeydev11 <163130001+honeydev11@users.noreply.github.com>
2024-03-12 19:34:38 +0500

    change backend url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/auth",
+    baseUrl: "http://3.18.193.66:5000/api/v1/auth",
   }),
   endpoints: (builder) => ({
     authenticate: builder.mutation<any, { endpoint: string; body: any }>({
--- src/services/task.ts
+++ src/services/task.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const taskApi = createApi({
   reducerPath: "taskApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/task",
+    baseUrl: "http://3.18.193.66:5000/api/v1/task",
     prepareHeaders: (headers) =>
       headers.set("Authorization", `Bearer ${localStorage.getItem("token")}`),
   }),
