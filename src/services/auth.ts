6c24445f3758aa9556fa8c8c88f9aae689ea6265
honeydev11 <163130001+honeydev11@users.noreply.github.com>
2024-03-15 11:59:49 +0500

    api url change

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://3.18.193.66:5000/api/v1/auth",
+    baseUrl: "/api/v1/auth",
   }),
   endpoints: (builder) => ({
     authenticate: builder.mutation<any, { endpoint: string; body: any }>({
